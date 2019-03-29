6cc711b0ebaf69563da69de08189f9d6d330e868
James Basile <jbasile145@gmail.com>
2019-03-29 13:32:27 -0400

    GET and POST for projects succesful, seeds also successful

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ server.use(helmet());
 
 const db = require('./data/dbConfig');
 
+server.get('/', (req, res) => {
+    res.send('James Basile: BackEnd Week 2 Sprint Challenge')
+})
+
+//GET all projects
+server.get('/api/projects', (req, res) => {
+    db('projects')
+        .then(projects => res.status(200).json(projects))
+        .catch(err => res.status(500).json(err));
+})
+
+//Post a new project
+server.post('/api/projects', (req, res) => {
+    db('projects')
+        .insert(req.body)
+        .then( project => res.status(201).json(project))
+        .catch(err => res.status(500).json(err));
+})
+
 const port = 5000;
 
 server.listen(port, () => {
