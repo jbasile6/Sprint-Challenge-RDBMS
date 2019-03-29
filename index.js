971c45bcc63e5ccbefa1d513f8662ab21cb1d418
James Basile <jbasile145@gmail.com>
2019-03-29 12:27:33 -0400

    setup knexfile, starting to add seeds and migrations

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+
+
+
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`\n** API running on http://localhost:${port} **\n`)
+})
